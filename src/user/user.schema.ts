345ba7efea8442abd0b44e3b1ccb992d69010ba9
Adel Muhammad <dolaadel574@gmail.com>
2026-05-13 00:22:38 +0300

    update user schema

--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -5,8 +5,73 @@ export type UserDocument = HydratedDocument<User>;
 
 @Schema({ timestamps: true })
 export class User {
-  @Prop()
+  @Prop({
+    type: String,
+    required: true,
+    min: [3, 'Name must be at least 3 characters'],
+    max: [30, 'Name must be at most 30 characters'],
+  })
   name: string;
+
+  @Prop({
+    type: String,
+    required: true,
+    unique: true,
+  })
+  email: string;
+
+  @Prop({
+    type: String,
+    required: true,
+    min: [5, 'Password must be at least 3 characters'],
+    max: [30, 'Password must be at most 30 characters'],
+  })
+  password: string;
+
+  @Prop({
+    type: String,
+    required: true,
+    enum: ['user', 'admin'],
+    default: 'user',
+  })
+  role: string;
+
+  @Prop({
+    type: String,
+  })
+  avatar: string;
+
+  @Prop({
+    type: Number,
+  })
+  age: number;
+
+  @Prop({
+    type: String,
+  })
+  phoneNumber: string;
+
+  @Prop({
+    type: String,
+  })
+  address: string;
+
+  @Prop({
+    type: Boolean,
+    default: true,
+  })
+  active: string;
+
+  @Prop({
+    type: String,
+  })
+  verificationCode: string;
+
+  @Prop({
+    type: String,
+    enum: ['male', 'female'],
+  })
+  gender: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
